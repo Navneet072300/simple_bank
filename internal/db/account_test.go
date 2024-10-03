459ff9b5ba513222cd378fe62c0fb7c54894f84e
Navneet Shahi <navneetshahi345@gmail.com>
2024-10-04 00:12:35 +0530

    "Refactor database schema and queries: updated `transfers` table, added foreign keys, modified SQL queries, and updated Go code to match changes."

--- internal/db/account_test.go
+++ internal/db/account_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/internal/db/util"
+	"github.com/techschool/simplebank/internal/util"
 )
 
 func createRandomAccount(t  *testing.T) Account{
