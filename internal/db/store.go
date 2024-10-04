165dbffa6964ab56a6585da05cee0d431b2aab1a
Navneet Shahi <navneetshahi345@gmail.com>
2024-10-05 01:30:31 +0530

    "Refactor TransferTx function to use addMoney helper function and add TestTransferTxDeadlock test case"

--- internal/db/store.go
+++ internal/db/store.go
@@ -85,34 +85,37 @@ func  (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Transfer
 				return err
 				}
             
-			account1, err :=  q.GetAccount(ctx, arg.FromAccountID)
-			if err != nil {
-				return err
-				}
-		    
-		    result.FromAccount, err =   q.UpdateAccount(ctx, UpdateAccountParams{
-				ID: arg.FromAccountID,
-				Balance: account1.Balance - arg.Amount,
-			})
-			if err != nil {
-				return err
-				}
-
-			account2, err :=  q.GetAccount(ctx, arg.ToAccountID)
-			if err != nil {
-				return err
-				}
-
-		    result.ToAccount, err =   q.UpdateAccount(ctx, UpdateAccountParams{
-				ID: arg.ToAccountID,
-				Balance: account2.Balance + arg.Amount,
-			})
-			if err != nil {
-				return err
-				}
+			if arg.FromAccountID < arg.ToAccountID {
+		       result.FromAccount, result.ToAccount, err= addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+			}else{
+				result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+			}
 
 		return nil
 		})
 
 		return  result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2  int64,
+	amount2 int64,
+) (account1  Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return 
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID2,
+		Amount: amount2,
+	})
+	return
 }
