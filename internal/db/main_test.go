c01ffdda9e728ba1f14cdfc277f0e56e09e73c8e
Navneet Shahi <navneetshahi345@gmail.com>
2024-10-06 12:43:39 +0530

    Update main_test.go

--- internal/db/main_test.go
+++ internal/db/main_test.go
@@ -22,10 +22,10 @@ func TestMain(m *testing.M){
 	var err  error
 	testDB, err = sql.Open(dbDriver, dbSource)
 	
-
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
+	
 	testQueries = New(testDB)
 
 	os.Exit(m.Run())
